5a34f196c531529d83490b673f6ee40bd782da2f
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-11-20 00:44:13 -0500

    make sure modules are up to date

--- flake.nix
+++ flake.nix
@@ -12,7 +12,8 @@
           set -x
           cp -r ${modules} node_modules
           cp -r ${./src} src
-          find 
+          cp ${./bun.lockb} bun.lockb
+          sha256sum -c node_modules/sum
           test -f src/index.ts
           mkdir dist
           bun ${./build.ts}
@@ -21,7 +22,7 @@
       '';
       modules = pkgs.runCommand "node_modules" {
         buildInputs = [pkgs.nodejs pkgs.bun];
-        outputHash = sha256:NuU1lHWPUA8+35MELnG7du3vkZCeWl3Mbq5xrzFBUBs=;
+        outputHash = sha256:hSQM+6MAsvj2IkS5pohSv7xn9BvS4pOqzBpPSuUnSRk=;
         outputHashMode = "recursive";
       } ''
         cp ${./package.json} package.json
@@ -32,6 +33,7 @@
         echo >> bunfig.toml "disable = true"
         NODE_TLS_REJECT_UNAUTHORIZED=0 bun install
         mv node_modules $out
+        sha256sum bun.lockb > $out/sum
       '';
     }) nixpkgs.legacyPackages;
   };
